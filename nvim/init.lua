63dad3d8e923b3b5337e074941127018bdc3285e
Gabriel Marin <gbrlmarn@gmail.com>
2022-09-15 15:33:52 +0300

    added old vim commands

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,3 @@
 require('plugins')
 require('binds')
+require('vim')
---
+++ nvim/lua/vim.lua
@@ -0,0 +1,10 @@
+vim.cmd('syntax on')
+vim.cmd('set number')
+vim.cmd('set ruler')
+vim.cmd('set ai')
+vim.cmd('set hlsearch')
+vim.cmd('set expandtab')
+vim.cmd('set tabstop=2')
+vim.cmd('set shiftwidth=2')
+vim.cmd('highlight Comment ctermfg=green')
+
