f706db322c8f015e1660fdb78151b8ff5e2928a7
Gabriel Marin <gbrlmarn@gmail.com>
2022-09-15 13:31:02 +0300

    added nvim config

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,12 @@
+-- This file can be loaded by calling 'lua require('plugins')' from
+-- init.vim
+
+-- Only required if you have packer configured as 'opt'
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+	use 'Olical/conjure'
+	use 'Olical/aniseed'
+end)
