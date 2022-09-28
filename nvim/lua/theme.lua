f9564160611962a9159ac1d95582adf9b61490da
Gabriel Marin <gbrlmarn@gmail.com>
2022-09-28 22:09:55 +0300

    modified nvim config

---
+++ nvim/lua/theme.lua
@@ -0,0 +1,12 @@
+vim.cmd('set background=dark')
+
+local dracula = require("dracula")
+dracula.setup({
+  -- customize dracula color palette
+  colors = {
+    bg = "#000000",
+    comment = "#008000"
+  }
+})
+
+vim.cmd('colorscheme dracula')
