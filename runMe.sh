c97ed322df3e759a53026f9304cb9c38a9f4d282
Gabriel Marin <gbrlmarn@gmail.com>
2022-10-09 12:04:32 +0300

    added mac randomizatoin for internet adapter

--- runMe.sh
+++ runMe.sh
@@ -3,6 +3,10 @@ cp ./.bash_profile ~/.bash_profile
 cp ./.tmux.conf ~/.tmux.conf
 cp ./.vimrc ~/.vimrc
 
-# install light for brightness control
+# install light for brightness control(pacman...)
 sudo pacman -S light
 sudo chmod +s /usr/bin/light
+
+# add macrandomization with NetworkManager
+sudo cp ./conf-files/00-macrandomize.conf ~/etc/NetworkManager/conf.d/
+sudo systemctl NetworkManager # Restart NetworkManager service
