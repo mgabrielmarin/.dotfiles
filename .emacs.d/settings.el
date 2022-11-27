4562dd63ae2f37c8f18c0998361578bafa78171c
Gabriel Marin <gbrlmarn@proton.me>
2022-11-27 13:47:34 +0200

    Added transparency and other packages...

--- .emacs.d/settings.el
+++ .emacs.d/settings.el
@@ -1,15 +1,15 @@
 (require 'package)
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                   ("melpa" . "https://melpa.org/packages/")))
-;;(add-to-list 'package-archives
-;;	     '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 (defvar my-packages '(;; Put packages here...
+		      ir-black-theme
+		      auto-complete
+		      geiser
 		      magit
-		      doom-themes
 		      bongo
 		      volume
 		      ))
@@ -17,24 +17,20 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
+(require 'auto-complete)
+(global-auto-complete-mode t)
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
-
-(setq inhibit-startup-message t)
-(setq initial-scratch-message nil)
-
-(setq initial-frame-alist
-'((top . 0) (left . -1) (width 60) (height . 80)))
-(setq display-time-day-and-date t)
-
-(set-face-attribute 'default nil :font "Liberation Mono-16")
-  (use-package ir-black-theme
-  :ensure t
-  :config
-  (load-theme 'ir-black t)
-
-;; Corects (and improves) org-mode' native fontificaion.
-(doom-themes-org-config))
+(setq inhibit-startup-screen t)
 
 (display-time-mode)
+  (set-face-attribute 'default nil :font "Liberation Mono-16")
+  (use-package ir-black-theme
+    :ensure t
+    :config
+    (load-theme 'ir-black t))
+  ;; Set transparency
+(set-frame-parameter (selected-frame) 'alpha '(70 70))
+(add-to-list 'default-frame-alist '(alpha 70 70))
