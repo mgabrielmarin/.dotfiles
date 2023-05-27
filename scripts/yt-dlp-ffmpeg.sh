247d418a5a383154c65628d23b3acc63e1630b24
Gabriel Marin <gbrlmarn@gmail.com>
2023-05-28 02:26:21 +0300

    Script for downloading and cutting yt content

---
+++ scripts/yt-dlp-ffmpeg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo Please enter URL:
+read url
+echo Please enter time start[HH:MM:SS.mm]
+read start 
+echo Please enter time stop[HH:MM:SS.mm]
+read stop 
+yt-dlp $url --downloader ffmpeg --downloader-args "ffmpeg_i:-ss $start -to $stop" 
+echo "Thanks :)"
