d6ecc4b8d146dddea78bb4df5f2ce034acee2a7d
Gabriel Marin <gbrlmarn@gmail.com>
2023-08-07 19:24:18 +0300

    emacs: Added custom.el

--- init.el
+++ init.el
@@ -210,17 +210,10 @@
   (setq rmh-elfeed-org-files
         (list "~/.emacs.d/elfeed.org")))
 
+;; config changes made through the customize UI will be stored here
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+(when (file-exists-p custom-file)
+  (load custom-file))
+
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(helm-emms emms-player-simple-mpv emms elfeed-org elfeed ivy company markdown-mode erlang elixir-mode flycheck-joker cider inf-clojure clojure-mode rainbow-mode rainbow-delimiters exec-path-from-shell paredit diminish dracula-theme use-package magit)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
