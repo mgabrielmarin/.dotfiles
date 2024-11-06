9aad73246b37e6b3f8b3831e301d0afe942450d4
Gabriel Marin <marin.gabriel@protonmail.com>
2024-11-06 23:41:55 +0200

    Added installation link(written with dvorak layout)

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # My configuration
 
 Programs that I use and like 
+
+## Install
+Use `link.sh` for installation
---
+++ link.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -i -s /bash/.bashrc ~/.bashrc
+ln -i -s /bash/.bash_profile ~/.bash_profile
+ln -i -s /bash/.bash_aliases ~/.bash_aliase
+ln -i -s .vimrc ~/.vimrc
+ln -i -s /nvim ~/.config/nvim
+ln -i -s /alacritty ~/.config/alacritty
+ln -i -s /redshift ~/.config/redshift
