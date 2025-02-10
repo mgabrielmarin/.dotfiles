f68f2dee6eeaad515e01b2777f4160e7f58a3e77
Gabriel Marin <gbrlmarn@gmail.com>
2025-02-11 00:14:51 +0200

    nvim: show hidden files in telescope 'find_files'

--- nvim/lua/gbrlmarn/lazy/telescope.lua
+++ nvim/lua/gbrlmarn/lazy/telescope.lua
@@ -8,13 +8,20 @@ return {
     },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            pickers = {
+                find_files = {
+                    hidden = true
+                }
+
+            }
+        })
 
         local builtin = require('telescope.builtin')
 
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Telescope find files'})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = 'Telescope help tags'})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = 'Telescope live grep'})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Telescope buffers'})
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     end
 }
