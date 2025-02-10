74b3978120d752c8d5c301986fc891d5f7ea5834
Gabriel Marin <gbrlmarn@gmail.com>
2025-02-10 20:22:30 +0200

    nvim: added dracula theme and removed xcode and cattpuccin themes

--- nvim/lua/gbrlmarn/remap.lua
+++ nvim/lua/gbrlmarn/remap.lua
@@ -4,7 +4,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "ff", vim.lsp.buf.format)
 
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
