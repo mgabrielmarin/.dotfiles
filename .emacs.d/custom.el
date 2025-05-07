9ab25387bf91beec49c15dd85a604641a4e31ee2
Gabriel Marin <gbrlmarn@gmail.com>
2025-05-07 20:13:33 +0300

    emacs: added 'custom.el'

---
+++ .emacs.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(company geiser-guile geiser pinentry bash-completion wgrep undo-tree org-drill-table org-drill emms elfeed-org elfeed vertico orderless corfu markdown-mode typescript-mode go-mode elixir-mode erlang slime cider inf-clojure clojure-mode rainbow-mode rainbow-delimiters pdf-tools exec-path-from-shell paredit diminish catppuccin-theme magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
